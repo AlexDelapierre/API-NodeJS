59a30149a3268eca1e9d83f0f0c33402a952ee28
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-04-10 16:49:25 +0200

    connect Atlas mongodb BDD in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,38 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Alex:uVnzgp0XqhuW0Wug@cluster0.uj09drm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// mongoose.connect('mongodb+srv://Alex:<uVnzgp0XqhuW0Wug>@cluster0.uj09drm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+//   { useNewUrlParser: true,
+//     useUnifiedTopology: true })
+//   .then(() => console.log('Connexion à MongoDB réussie !'))
+//   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());   
 
