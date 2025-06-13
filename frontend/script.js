5976cb4e43004008ff7c238ab5bd4dfd4ec95036
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-06-13 18:57:53 +0200

    Add vendre.html

---
+++ frontend/script.js
@@ -0,0 +1,11 @@
+function loadNavbar() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Erreur lors du chargement de la navbar:', error));
+}
+
+// On peut appeler la fonction directement au chargement de la page
+document.addEventListener('DOMContentLoaded', loadNavbar);
