a0a718281fc62ed1f1e948566753d9499e7597b2
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-06-18 19:29:54 +0200

    Add router/stuff.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Thing = require('./models/Thing');
+const stuffRoutes = require('./routes/stuff');
 
 const app = express();
 app.use(express.json());
@@ -15,60 +15,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routes
-app.get('/api/stuff', (req, res) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => {
-      if (!thing) {
-        return res.status(404).json({ message: 'Objet non trouvé' });
-      }
-      res.status(200).json(thing);
-    })
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.post('/api/stuff', (req, res) => {
-  const thing = new Thing({...req.body});
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré en base de données !' }))
-    .catch((error) => {
-      console.error('Erreur lors de l\'enregistrement :', error);
-      res.status(400).json({ error });
-    });
-});
-
-// Update endpoint avec Thing.findByIdAndUpdate()
-app.put('/api/stuff/:id', (req, res) => {
-  Thing.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    .then(updatedThing => {
-      if (!updatedThing) {
-        return res.status(404).json({ message: 'Objet non trouvé' });
-      }
-      res.status(200).json(updatedThing);
-    })
-    .catch(error => {
-      console.error('Erreur lors de la mise à jour :', error);
-      res.status(400).json({ error });
-    });
-});
-
-// Update endpoint avec Thing.updateOne()
-// app.put('/api/stuff/:id', (req, res, next) => {
-//   Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-//     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-//     .catch(error => res.status(400).json({ error }));
-// });
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
