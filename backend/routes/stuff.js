ef6a6663fa611d15680728bd8ff08bc7ca10176a
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-06-27 15:37:12 +0200

    update backend/routes/stuff.js

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.get('/', auth, stuffCtrl.getAllThings);
-router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', stuffCtrl.getAllThings);
+router.get('/:id', stuffCtrl.getOneThing);
 router.post('/', auth, stuffCtrl.createThing);
 router.put('/:id', auth, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
