fec40ac432e72cfe548185d507d53ca8ee8741cd
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2025-07-29 17:16:16 +0200

    chore: delete api.d.ts

--- frontend/src/services/api.d.ts
+++
@@ -1,5 +0,0 @@
-// Déclarations de types pour services/api.js
-import { Objet } from '../types/objet';
-
-export function getObjets(): Promise<Objet[]>;
-// Ajoutez ici d'autres signatures si besoin (ajouterObjet, getObjetById, etc.)
